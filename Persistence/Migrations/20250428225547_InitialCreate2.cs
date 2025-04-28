8b9e2af8803f126bcac5aa8221261a7d72b0eea0
LFCode81 <ammunitionvx81@gmail.com>
2025-04-28 19:59:34 -0400

    End of section 2

---
+++ Persistence/Migrations/20250428225547_InitialCreate2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCancelled",
+                table: "Activities");
+
+            migrationBuilder.RenameColumn(
+                name: "Lattitude",
+                table: "Activities",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "Activities",
+                newName: "Lattitude");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCancelled",
+                table: "Activities",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
