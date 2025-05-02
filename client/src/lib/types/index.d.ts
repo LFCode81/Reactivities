7d24dafb5e0574a817dc96468a747cd32b59d36e
LFCode81 <ammunitionvx81@gmail.com>
2025-05-01 21:07:18 -0400

    End Of Section 3

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,11 @@
+type Activity = {
+    id: string
+    title: string
+    description: string
+    category: string
+    city: string
+    venue: string
+    date: string
+    latitude: number
+    longitude: number
+}
