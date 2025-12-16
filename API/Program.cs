979d09a0008d7b487ca5bf4416cee69a4bb71364
LFCode81 <144384718+LFCode81@users.noreply.github.com>
2025-12-15 20:06:51 -0500

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +16,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddCors();
-
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
